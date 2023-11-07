b0ffcf8dfe053d7b859cfe8c99086153b517b03e
AmarieB07 <149451184+AmarieB07@users.noreply.github.com>
2023-11-06 19:04:48 -0800

    Update main.py

--- main.py
+++ main.py
@@ -7,6 +7,13 @@ def main():
     total 
     average
     """
+    score1 = int(input('Enter the score 1: '))
+    score2 = int(input('Enter the Score 2: '))
+    score3 = int(input('Enter the score 3: '))
+    total = score1 + score2+ score3
+    average = total / 3
+    print (f'Total is  {total:10d}' )
+    print (f'Average is {average:.2f}')
 
     ########################################
     # Do not delete the return statement
